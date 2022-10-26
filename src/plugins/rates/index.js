e69195ce74a0bfe78282e6079ebe45497f1642a2
Aleksandr Kukharenko <aleksandr@titan.io>
2022-10-26 15:42:23 +0300

    feat: rate plugin

--- src/plugins/rates/index.js
+++ src/plugins/rates/index.js
@@ -1,17 +1,21 @@
-'use strict';
+'use strict'
 
-const { getExchangeRate } = require('safe-exchange-rate');
-const debug = require('debug')('lmr-wallet:core:rates');
+const axios = require('axios').default;
+const { getExchangeRate } = require('safe-exchange-rate')
+const debug = require('debug')('lmr-wallet:core:rates')
+const { BittrexClient } = require('bittrex-node')
 
-const createStream = require('./stream');
+
+const createStream = require('./stream')
+const client = new BittrexClient()
 
 /**
  * Create a plugin instance.
  *
  * @returns {({ start: Function, stop: () => void})} The plugin instance.
  */
-function createPlugin () {
-  let dataStream;
+function createPlugin() {
+  let dataStream
 
   /**
    * Start the plugin instance.
@@ -19,60 +23,78 @@ function createPlugin () {
    * @param {object} options Start options.
    * @returns {{ events: string[] }} The instance details.
    */
-  function start ({ config, eventBus }) {
-    debug.enabled = debug.enabled || config.debug;
+  function start({ config, eventBus }) {
+    debug.enabled = debug.enabled || config.debug
 
-    debug('Plugin starting');
+    debug('Plugin starting')
 
-    const { ratesUpdateMs, symbol } = config;
+    const { ratesUpdateMs, symbol } = config
+
+    const getAsset = (id) =>
+    axios
+      .get(`https://api.coingecko.com/api/v3/simple/price`, {
+        params: {
+          ids: 'lumerin',
+          vs_currencies: 'usd'
+        }
+      })
+      .then((response) =>
+        response.data &&
+        response.data.lumerin &&
+        typeof response.data.lumerin.usd === 'number'
+          ? Number.parseFloat( response.data.lumerin.usd)
+          : null
+      )
 
     const getRate = () =>
-    // TODO: Update LMR/ETH or LMR/USD rate whenever we get put onto a dex
+      // TODO: Update LMR/ETH or LMR/USD rate whenever we get put onto a dex
       // getExchangeRate(`${symbol}:USD`).then(function (rate) {
-      getExchangeRate(`${symbol}:USD`).then(function (rate) {
-        if (typeof rate !== 'number') {
-          throw new Error(`No exchange rate retrieved for ${symbol}`);
-        }
-        return rate;
-      });
+      {
+        return symbol === 'LMR' ? getAsset() : getExchangeRate(`LMR:USD`).then(function (rate) {
+          if (typeof rate !== 'number') {
+            throw new Error(`No exchange rate retrieved for ${symbol}`)
+          }
+          return rate
+        })
+      }
 
-    dataStream = createStream(getRate, ratesUpdateMs);
+    dataStream = createStream(getRate, ratesUpdateMs)
 
     dataStream.on('data', function (price) {
-      debug('Coin price received');
+      debug('Coin price received')
 
-      const priceData = { token: symbol, currency: 'USD', price };
-      eventBus.emit('coin-price-updated', priceData);
-    });
+      const priceData = { token: symbol, currency: 'USD', price }
+      eventBus.emit('coin-price-updated', priceData)
+    })
 
     dataStream.on('error', function (err) {
-      debug('Data stream error');
+      debug('Data stream error')
 
       eventBus.emit('wallet-error', {
         inner: err,
         message: `Could not get exchange rate for ${symbol}`,
-        meta: { plugin: 'rates' }
-      });
-    });
+        meta: { plugin: 'rates' },
+      })
+    })
 
     return {
-      events: ['coin-price-updated', 'wallet-error']
-    };
+      events: ['coin-price-updated', 'wallet-error'],
+    }
   }
 
   /**
    * Stop the plugin instance.
    */
-  function stop () {
-    debug('Plugin stopping');
+  function stop() {
+    debug('Plugin stopping')
 
-    dataStream.destroy();
+    dataStream.destroy()
   }
 
   return {
     start,
-    stop
-  };
+    stop,
+  }
 }
 
-module.exports = createPlugin;
+module.exports = createPlugin
