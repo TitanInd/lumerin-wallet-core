ecb9c764e42993c9c1d0dfe933c96114529e7dc2
Aleksandr Kukharenko <aleksandr@titan.io>
2023-02-02 22:30:33 +0200

    feat: pass user-agent for etherscan requests

--- src/plugins/explorer/explorer.js
+++ src/plugins/explorer/explorer.js
@@ -20,6 +20,9 @@ const createExplorer = (chainId, web3, lumerin) => {
   }
   const api = axios.create({
     baseURL,
+    headers: { 
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+    }
   })
 
   return new Explorer({ api, lumerin, web3 })
