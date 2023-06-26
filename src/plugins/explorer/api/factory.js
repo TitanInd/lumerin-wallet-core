194965f2815a5e04de7d9827b3580508a7433a17
Aleksandr Kukharenko <aleksandr@titan.io>
2023-06-26 16:52:56 +0300

    fix: use correct difficulty api; add arbitrum explorer

---
+++ src/plugins/explorer/api/factory.js
@@ -0,0 +1,30 @@
+const { createArbiscanApi } = require('./arbiscan-factory')
+const { createBlockscoutApi } = require('./blockscout-factory')
+const { createEtherscanApi } = require('./etherscan-factory')
+
+const createExplorerApis = (chainId) => {
+  const apis = []
+
+  switch (chainId.toString()) {
+    case 'mainnet':
+    case '1':
+    case 'goerli':
+    case '5':
+    case 'sepolia':
+    case '11155111':
+      const etherscanApi = createEtherscanApi(chainId)
+      const blockscoutApi = createBlockscoutApi(chainId)
+      apis.push(etherscanApi, blockscoutApi)
+      break;
+    case '421613':
+      const arbiscanApi = createArbiscanApi(chainId)
+      apis.push(arbiscanApi)
+      break
+    default:
+      throw new Error(`Unsupported chain ${chainId}`)
+  }
+
+  return apis;
+}
+
+module.exports = { createExplorerApis }
